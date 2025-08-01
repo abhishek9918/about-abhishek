3e80cc5462b147fa25ba7ff1c255e9179fca393a
@bhishekOjha <ojha.abhishek312@gmail.com>
2025-08-01 15:17:17 +0530

    rebase

--- src/app/components/projects/works.ts
+++ src/app/components/projects/works.ts
@@ -43,3 +43,27 @@ export interface worksInterface {
   url: string;
   img: string;
 }
+
+export const slickConfig = {
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots: true,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
+};
