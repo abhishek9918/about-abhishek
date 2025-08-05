7d73a4fc1dd7079718b03a8c6f0c561351006a3e
@bhishekOjha <ojha.abhishek312@gmail.com>
2025-08-05 19:19:11 +0530

    new updated

--- src/app/core/services/text-animator.ts
+++ src/app/core/services/text-animator.ts
@@ -6,29 +6,56 @@ declare const anime: any;
 })
 export class TextAnimator {
   animateLetters(selector: string): void {
-    const textWrapper = document.querySelector(selector);
-    if (!textWrapper || !textWrapper.textContent) return;
+    // const textWrapper = document.querySelector(selector);
+    // if (!textWrapper || !textWrapper.textContent) return;
 
+    // textWrapper.innerHTML = textWrapper.textContent.replace(
+    //   /\S/g,
+    //   "<span class='letter'>$&</span>"
+    // );
+
+    const textWrapper = document.querySelector('.ml12');
+    if (!textWrapper || !textWrapper.textContent) return;
     textWrapper.innerHTML = textWrapper.textContent.replace(
       /\S/g,
       "<span class='letter'>$&</span>"
     );
 
+    // anime
+    //   .timeline({ loop: true })
+    //   .add({
+    //     targets: `${selector} .letter`,
+    //     scale: [0, 1],
+    //     duration: 1500,
+    //     elasticity: 600,
+    //     delay: (el: any, i: number) => 45 * (i + 1),
+    //   })
+    //   .add({
+    //     targets: selector,
+    //     opacity: 0,
+    //     duration: 1000,
+    //     easing: 'easeOutExpo',
+    //     delay: 1000,
+    //   });
+
     anime
       .timeline({ loop: true })
       .add({
-        targets: `${selector} .letter`,
-        scale: [0, 1],
-        duration: 1500,
-        elasticity: 600,
-        delay: (el: any, i: number) => 45 * (i + 1),
+        targets: '.ml12 .letter',
+        translateX: [40, 0],
+        translateZ: 0,
+        opacity: [0, 1],
+        easing: 'easeOutExpo',
+        duration: 1200,
+        delay: (el: any, i: any) => 500 + 30 * i,
       })
       .add({
-        targets: selector,
-        opacity: 0,
-        duration: 1000,
-        easing: 'easeOutExpo',
-        delay: 1000,
+        targets: '.ml12 .letter',
+        translateX: [0, -30],
+        opacity: [1, 0],
+        easing: 'easeInExpo',
+        duration: 1100,
+        delay: (el: any, i: any) => 100 + 30 * i,
       });
   }
 
@@ -40,6 +67,7 @@ export class TextAnimator {
         "<span class='letter'>$&</span>"
       );
       const lineWidth = textWrapper?.getBoundingClientRect().width ?? 0;
+
       const paddingLeft = parseFloat(
         getComputedStyle(textWrapper!).paddingLeft || '0'
       );
