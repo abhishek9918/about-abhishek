82393438383db1b5c80a33fc7650de109c3653ac
@bhishekOjha <ojha.abhishek312@gmail.com>
2025-08-11 13:08:45 +0530

    implemented loader

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,19 @@ import { App } from './app/app';
 import { register } from 'swiper/element/bundle';
 
 register();
-bootstrapApplication(App, appConfig).catch((err) => console.error(err));
+bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err))
+  .finally(() => {
+    const loader = document.getElementById('app-loader');
+    console.log('Loader found?', loader !== null);
+
+    if (loader) {
+      loader.style.opacity = '0';
+      loader.style.transition = 'opacity 0.5s ease';
+      // remove after fade-out animation
+      setTimeout(() => {
+        loader.remove();
+        //   console.log('Loader removed?', !document.getElementById('app-loader'));
+      }, 500);
+    }
+  });
